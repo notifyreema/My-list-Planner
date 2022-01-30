103ca8e71ae0c2036a562d15a15f3a431a415791
jyothsnaPeruri <95264561+jyothsnaPeruri@users.noreply.github.com>
2022-01-31 10:46:24 +1100

    Update taskManager.js
    added html file in taskmanager file

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,28 +1,22 @@
 const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
-  const html = `<li class="card" style="min-width: 50vw">
-  <div class="card-body">
-    <h5 class="card-title">${name}</h5>
-    <p class="card-text">
-      ${description}
-    </p>
-    <p class="card-text">${assignedTo} To</p>
-    <p class="card-text">${dueDate}</p>
-    <div class="card-footer row">
-      <div class="col-6">
-        <p class="card-text"><b>${status}</b></p>
-      </div>
-      <div class="col-3">
-        <button class="btn btn-outline-success done-button">
-          Done
-        </button>
-      </div>
-      <div class="col-3">
-        <button class="btn btn-outline-danger delete-button">
-          Delete
-        </button>
-      </div>
+  const html = `<li class="list-group-item">
+  <div class="d-flex w-500 mt-2 justify-content-between align-items-center">
+    <h5 class="card-title"> Name : ${name}</h5>
+    <span class="badge badge-info"> ${status}</span>
     </div>
+    <div class="d-flex w-100 mb-3 justify-content-between">
+    <small>AssignedTo : ${assignedTo}</small>
+    <small>DueDate : ${dueDate}</small>
   </div>
+    <p class="card-text">
+      Description : ${description}
+    </p>
+   
+    <p>
+    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal"
+    data-target="#editForm">EDIT</button>
+  <button class="delete btn btn-danger btn-sm" name="delete">DELETE</button>
+</p>
 </li>`;
   return html;
 };
@@ -50,21 +44,21 @@ class TaskManager {
       status: status,
     };
 
-this.tasks.push(task);
+    this.tasks.push(task);
 
   }
 
   render() {
     let tasksHtmlList = [];
     // Loop over our tasks and create the html, storing it in the array
-    
+
     for (let i = 0; i < this.tasks.length; i++) {
       // Get the current task in the loop
       const task = this.tasks[i];
       // Format the date
-      
+
       const date = new Date(task.dueDate);
-    
+
       //console.log("date is "+date)
       const formattedDate =
         date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
@@ -89,10 +83,11 @@ this.tasks.push(task);
     // Set the inner html of the tasksList on the page
     const tasksList = document.querySelector("#task-list");
     tasksList.innerHTML = tasksHtml;
+
+
   }
- 
+
 
 
 
 }
-
